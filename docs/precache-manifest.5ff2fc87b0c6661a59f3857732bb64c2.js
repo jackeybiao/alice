a3dc5497e17c93669ad8a4c059246150c14e68a3
hanbiao <hanb@deepblueai.com>
2020-01-09 17:02:11 +0800

    finish

---
+++ docs/precache-manifest.5ff2fc87b0c6661a59f3857732bb64c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b084df5a0725a15b585a9b87f5028dd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9c901a528632374af1a",
+    "url": "./static/css/main.75997cfb.chunk.css"
+  },
+  {
+    "revision": "3795d0423b5eecbb517c",
+    "url": "./static/js/2.43c23665.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "./static/js/2.43c23665.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9c901a528632374af1a",
+    "url": "./static/js/main.568c77c8.chunk.js"
+  },
+  {
+    "revision": "85d63860e7f0346e71f4",
+    "url": "./static/js/runtime-main.7bcf4d51.js"
+  }
+]);
